0bd45ab19555bb88bcd5fe1387915c0bf000826a
Hugo Flores Perez <hugofloresperez@Hugos-MacBook-Pro.local>
2020-09-26 18:37:47 -0400

    Main VC

--- CircularTimerExample/ViewController.swift
+++ CircularTimerExample/ViewController.swift
@@ -6,14 +6,29 @@
 //  Copyright © 2020 Hugo Flores Perez. All rights reserved.
 //
 
+import Lottie
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    private let timerView = CircularTimerView(time: 11)
+    
+    let myView = LottieAnimatable<Int>(file: "timer_lottie", maximumValue: 10)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         view.backgroundColor = .red
+
+//        view.addSubview(timerView)
+//        timerView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        timerView.leftAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        timerView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -16).isActive = true
+//        timerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 32).isActive = true
+//        timerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16).isActive = true
+//
+//        timerView.start()
     }
 }
 
